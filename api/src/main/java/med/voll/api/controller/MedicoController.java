72fb5bd411b9512c95aeecbc30eefbac64f8712a
lfelipedemelo <lfelipedemelo@hotmail.com>
2023-10-09 18:03:19 -0300

    Inclusão de repository,controllers e VO's e versionamento de banco com flyway

---
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,28 @@
+package med.voll.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.Medico;
+
+@RestController
+@RequestMapping("medico")
+public class MedicoController {
+	
+	@Autowired
+	private MedicoRepository repository;
+
+	@PostMapping
+	@Transactional
+	public void cadastrar (@RequestBody @Valid DadosCadastroMedico dadosCadastroMedico) {
+		repository.save(new Medico(dadosCadastroMedico));
+	}
+	
+}
