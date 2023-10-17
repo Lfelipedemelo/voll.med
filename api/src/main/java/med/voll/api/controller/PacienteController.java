d533871297866a8bfac27d65041fda9860dbf26b
lfelipedemelo <lfelipedemelo@hotmail.com>
2023-10-17 12:27:31 -0300

    Criação de listagem de paciente e medico

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,6 +1,10 @@
 package med.voll.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +19,18 @@ import med.voll.api.paciente.PacienteRepository;
 @RestController
 @RequestMapping("paciente")
 public class PacienteController {
-	
+
 	@Autowired
 	private PacienteRepository repository;
 
 	@PostMapping
 	@Transactional
-	public void cadastrar (@RequestBody @Valid DadosCadastroPaciente dadosCadastroPaciente) {
+	public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dadosCadastroPaciente) {
 		repository.save(new Paciente(dadosCadastroPaciente));
+	}
+
+	@GetMapping
+	public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, sort = { "nome" }) Pageable paginacao) {
+		return repository.findAll(paginacao).map(DadosListagemPaciente::new);
 	}
 }
